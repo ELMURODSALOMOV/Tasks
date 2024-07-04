526541ba0ce6d9b12cf39547960c443eb29e1056
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-07-04 19:31:37 +0500

    INFRA: Task There Impliment

---
+++ Models/Boxer.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasks.Models
+{
+    internal class Boxer
+    {
+       public Guid Id { get; set; }
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
+       public int Age { get; set; }
+       public int Weight { get; set; }
+    }
+}
